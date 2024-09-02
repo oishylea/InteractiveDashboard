62e87df8d4bd003127084b5e4ef65c40f128fd53
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-09-03 03:42:20 +0800

    upload data

--- dashboard.py
+++ dashboard.py
@@ -8,3 +8,14 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample SuperStore EDA")
+st.markdown('<style>div.block-container{padding-top:2rem}</style>',unsafe_allow_html=True)
+
+#Upload data
+fl=st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df=pd.read_csv(filename)
+else:
+    os.chdir(r"C:\Users\Izzah Alia\Documents\App develop\InteractiveDashboard")
+    df=pd.read_csv("Superstore.csv")
