707089a479a08298fe7f91572afe13bb46e168b5
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-09-03 03:34:07 +0800

    dashboard

--- dashboard.py
+++ dashboard.py
@@ -4,3 +4,7 @@ import pandas as pd
 import os
 import warnings
 warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
+
+st.title(" :bar_chart: Sample SuperStore EDA")
